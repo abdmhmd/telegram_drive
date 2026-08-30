126a97f520a13c36ddea8df96d28abe44ef1ff29
Abd AL Rhman Mohammed <124267092+abdmhmd@users.noreply.github.com>
2026-08-30 11:31:03 +0300

    FIX SOME ERORRES

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://teledrivebackend.pxxl.click',
+        target: 'http://localhost:3100',
         changeOrigin: true,
       },
     },
